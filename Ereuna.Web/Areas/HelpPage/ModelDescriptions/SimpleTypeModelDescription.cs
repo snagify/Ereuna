a2df50b4f6a229119a92e40113724b300cff9bb3
Steven Nagy <steven@snagy.name>
2015-05-10 15:45:20 +1000

    Created web project and unit tests as "Single Page Application" using VS 2015 RC. There's a lot of stuff in here and I'll slowly remove the stuff not needed. This is just to test the Github tools in VS

---
+++ Ereuna.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Ereuna.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Ereuna.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ereuna.Web.MvcApplication" Language="C#" %>
