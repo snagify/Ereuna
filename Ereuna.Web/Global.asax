1f286c3debc902b5d0468c408e8eb15193f61984
Steven Nagy <steven@snagy.name>
2015-05-13 19:36:23 +1000

    Decided to remove all the default crap that comes in those templates. Deleted everything, started again with empty project that just has MVC and Web API references.

--- Ereuna.Web/Global.asax
+++ Ereuna.Web/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ereuna.Web.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ereuna.Web.Global" Language="C#" %>
--- Ereuna.Web/Global.asax.cs
+++ Ereuna.Web/Global.asax.cs
@@ -1,37 +1,23 @@
-﻿using Autofac;
-using Autofac.Integration.Mvc;
-using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
 
 namespace Ereuna.Web
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start()
+        void Application_Start(object sender, EventArgs e)
         {
+            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-                        
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(BuildContainer()));
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
-
-        private IContainer BuildContainer()
-        {
-            var builder = new ContainerBuilder();
-            builder.RegisterControllers(typeof(WebApiApplication).Assembly);
-            // builder.RegisterModule<AutofacWebTypesModule>();  http://docs.autofac.org/en/latest/integration/mvc.html
-
-            // OWIN:  http://docs.autofac.org/en/latest/integration/owin.html
-
-            return builder.Build();
-        }
-
-
-        
     }
 }
