098a66483b851fd52de10670a1000561b6a88972
Steven Nagy <steven@snagy.name>
2015-05-15 18:14:26 +1000

    Cleanup

--- Ereuna.Web/Global.asax.cs
+++ Ereuna.Web/Global.asax.cs
@@ -1,23 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace Ereuna.Web
 {
-    public class Global : HttpApplication
+    public class EreunaWebApplication : HttpApplication
     {
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(BuildContainer()));
         }
+
+        private IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(EreunaWebApplication).Assembly);
+            // builder.RegisterModule<AutofacWebTypesModule>();  http://docs.autofac.org/en/latest/integration/mvc.html
+
+            // OWIN:  http://docs.autofac.org/en/latest/integration/owin.html
+
+            return builder.Build();
+        }
+
     }
 }
