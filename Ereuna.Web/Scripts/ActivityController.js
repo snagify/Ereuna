bcbd237b5781473ea8d5d2178e60fef278f5273c
snagify <steven@snagy.name>
2015-05-30 14:47:07 +1000

    Created my first Angular controller, loaded my recent actions web api feed, even added loading indicator. So easy in Angular!

---
+++ Ereuna.Web/Scripts/ActivityController.js
@@ -0,0 +1,15 @@
+﻿app.controller('ActivityController', function($scope, $http) {
+    var endpoint = 'api/activity';
+
+    $scope.loading = true;
+
+    $http({
+        method: 'GET',
+        url: endpoint
+    }).success(function (data, status) {
+        $scope.loading = false;
+        $scope.recentActivity = data;
+    }).error(function(data, status) {
+
+    });
+});
