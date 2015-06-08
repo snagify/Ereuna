3dbd1c87a415eb37f675d1c966bc62a86161c043
snagify <steven@snagy.name>
2015-06-08 16:19:18 +1000

    Implemented the server side verify of the Facebook token. Needs some refactoring but this is a good milestone.

--- Ereuna.Web/Controllers/LoginController.cs
+++ Ereuna.Web/Controllers/LoginController.cs
@@ -1,10 +1,20 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Net;
 using System.Web.Http;
 using Ereuna.Web.Data;
 using Ereuna.Web.Models;
+using Newtonsoft.Json;
 
 namespace Ereuna.Web.Controllers
 {
+    /// <summary>
+    /// The Facebook login process is a bit complex. I'll document it here for my own sanity as well.
+    /// First, we auth the user via facebook. This could be a login, or they could be returning and we just validate them.
+    /// Either way we end up with an access token. We submit this to the server.
+    /// We then verify the token via facebook again; this ensures the call to the API was from our app and it is valid for that user id.
+    /// We can then create a session around that user id and return a guid token for all future calls going forward (until session ends).
+    /// </summary>
     public class LoginController : ApiController
     {
         private readonly EreunaContext _context;
@@ -16,7 +26,18 @@ namespace Ereuna.Web.Controllers
 
         [HttpPost] public IHttpActionResult Post([FromBody] FacebookAccessToken token)
         {
-            var existingUser =_context.Users.FirstOrDefault(x => x.FacebookUserId == token.UserId);
+            if (IsTokenValid(token.AccessToken, token.UserId))
+            {
+                DoLogin(token);
+                return Ok();
+            }
+
+            return Unauthorized();
+        }
+
+        private void DoLogin(FacebookAccessToken token)
+        {
+            var existingUser = _context.Users.FirstOrDefault(x => x.FacebookUserId == token.UserId);
 
             if (existingUser != null)
             {
@@ -38,12 +59,30 @@ namespace Ereuna.Web.Controllers
 
                 _context.Users.Add(newUser);
             }
-            
-            _context.SaveChanges();
 
-            return Ok();
+            _context.SaveChanges();
         }
 
+        private bool IsTokenValid(string accessToken, string userId)
+        {
+            var apiTokenUrl = string.Format("https://graph.facebook.com/me?access_token={0}", accessToken);
+
+            var request = WebRequest.Create(apiTokenUrl);
+            request.Method = "GET";
+            var response = (HttpWebResponse)request.GetResponse();
+            using (var dataStream = response.GetResponseStream())
+            using (var reader = new StreamReader(dataStream))
+            {
+                var responseString = reader.ReadToEnd();
+                var facebookToken = JsonConvert.DeserializeObject<FacebookUserToken>(responseString);
+                if (facebookToken.Id == userId && facebookToken.Verified)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
         
 
     }
---
+++ Ereuna.Web/Models/FacebookUserToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ereuna.Web.Models
+{
+    public class FacebookUserToken
+    {
+        public string Id { get; set; } 
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Link { get; set; }
+        public string Locale { get; set; }
+        public string Name { get; set; }
+        public string Timezone { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public bool Verified { get; set; }
+    }
+}
