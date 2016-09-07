e97f6a9b11a5aa355fb68823420be1a76b9a8ed0
snagy <steven@snagy.name>
2016-09-07 22:08:19 +1000

    Moved the menu to the index and made it more angular.  Fixed up login and logout buttons. Added spinner for ajax waits.

---
+++ Ereuna.Web/ngControllers/IndexController.js
@@ -0,0 +1,111 @@
+﻿// http://ngmodules.org/modules/ngFacebook
+
+app.controller('IndexController', function ($rootScope, $scope, $http, $facebook, $window, $location) {
+    var endpoint = 'api/login';
+
+    $scope.loading = true;
+    
+    $scope.DoFacebookLogin = function() {
+        $facebook.login();
+    };
+
+    $scope.DoFacebookLogout = function () {
+        $facebook.logout();
+    };
+
+    $scope.DoFacebookDeauthentication = function () {
+        var url = '/me/permissions';
+        $facebook.api(url, 'DELETE').then(function (response) {
+            console.log(response);
+            if (response) {
+                $rootScope.userFullName = '';
+                $rootScope.LoginType = 'None';
+                $rootScope.IsLoggedIn = false;
+            }
+        });
+    };
+
+    function LoginToServer(user) {
+        var response = $facebook.getAuthResponse();
+        var token = {
+            UserId: response.userID,
+            AccessToken: response.accessToken,
+            ExpiresIn: response.expiresIn,
+            SignedRequest: response.signedRequest,
+            Email: user.email,
+            FirstName: user.first_name,
+            LastName: user.last_name
+        };
+        $http.post(endpoint, token).success(function (data, status, headers, config) {
+            console.log(data);
+            console.log(status);
+            console.log(headers);
+            console.log(config);
+            
+            $rootScope.SessionToken = data;
+            $window.sessionStorage.token = data;
+
+
+            $http({
+                method: 'GET',
+                url: 'api/projects'
+            }).success(function (data, status) {
+                console.log(data);
+                $rootScope.Projects = data;
+                $rootScope.HasProjects = true;
+            }).error(function (data, status) {
+                console.log(status);
+            });
+
+            // data contains the response
+            // status is the HTTP status
+            // headers is the header getter function
+            // config is the object that was used to create the HTTP request
+        }).error(function (data, status, headers, config) {
+            $rootScope.HasProjects = false;
+            $rootScope.IsLoggedIn = false;
+
+            console.log('Error occurred during login getting response from server');
+            console.log(data);
+            console.log(status);
+            console.log(headers);
+            console.log(config);
+        });
+    }
+    
+    $scope.$on('fb.auth.authResponseChange', function () {
+        $rootScope.IsLoggedIn = $facebook.isConnected();
+        if ($rootScope.IsLoggedIn) {
+            $rootScope.LoginType = 'FB';
+
+            $facebook.api('/me').then(function (user) {
+                $rootScope.userFullName = user.name;
+
+                LoginToServer(user);
+            });
+        }
+    });
+
+    $scope.$on('fb.load', function () {
+        $scope.loading = false;
+    });
+
+
+    function GetStyleClassForGlassMenu(menutarget) {
+        var current = $location.path().substr(1, menutarget.length+1);
+        var style = (current === menutarget) ? 'glass redglass' : 'glass blackglass';
+        return style;
+    }
+
+
+    $scope.LoginMenuClass = "glass blackglass";
+    $scope.HomeMenuClass = "glass blackglass";
+    $rootScope.$on("$locationChangeSuccess", function (event, next, current) {
+
+        $scope.LoginMenuClass = GetStyleClassForGlassMenu('login');
+        $scope.HomeMenuClass = GetStyleClassForGlassMenu('');
+    });
+
+
+
+});
---
+++ Ereuna.Web/ngControllers/LoginController.js
@@ -0,0 +1,35 @@
+﻿// http://ngmodules.org/modules/ngFacebook
+
+app.controller('LoginController', function ($rootScope, $scope, $http, $facebook, $window, $location) {
+
+
+    function GetStyleClassForGlassMenu(menutarget) {
+        var current = $location.path().substr(1, menutarget.length + 1);
+        var style = (current === menutarget) ? 'glass redglass' : 'glass blackglass';
+        return style;
+    }
+
+    //$rootScope.$on('$locationChangeSuccess'), function () {
+    //    $scope.LoginMenuClass = GetStyleClassForGlassMenu('login');
+    //    $scope.HomeMenuClass = GetStyleClassForGlassMenu('home');
+    //};
+
+
+    //$rootScope.$on('$locationChangeStart'), function () {
+    //    $scope.LoginMenuClass = GetStyleClassForGlassMenu('login');
+    //    $scope.HomeMenuClass = GetStyleClassForGlassMenu('home');
+
+    //};
+
+    //$rootScope.$on('$stateChangeStart'), function () {
+    //    $scope.LoginMenuClass = GetStyleClassForGlassMenu('login');
+    //    $scope.HomeMenuClass = GetStyleClassForGlassMenu('home');
+
+    //};
+
+    //$rootScope.$on('$stateChangeSuccess'), function () {
+    //    $scope.LoginMenuClass = GetStyleClassForGlassMenu('login');
+    //    $scope.HomeMenuClass = GetStyleClassForGlassMenu('home');
+
+    //};
+});
